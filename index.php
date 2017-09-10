edb177878908b85dd2d965130f488ce0e7334751
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-10 23:34:38 +0200

    testing different methods

--- index.php
+++ index.php
@@ -1,39 +1,41 @@
 <?php
 
-for ($i = 1; $i <= 70; $i++) {
+$names_array = ['Mook', 'Sarah', 'Maria'];
 
-  $names_array = ['Mook', 'Sarah', 'Maria'];
+$i = 1;
 
-  $ch = curl_init('https://badoo.com/en/dating/united-states/new-york/new-york/girls/page-' . $i . '/age-20-30/');
-  //$ch = curl_init('http://php.net/manual/en/book.curl.php');
+$ch = curl_init('https://badoo.com/en/dating/united-states/new-york/new-york/girls/page-' . $i . '/age-20-30/');
+//$ch = curl_init('http://php.net/manual/en/book.curl.php');
 
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-  $output = curl_exec($ch);
+$output = curl_exec($ch);
 
-  //echo $output;
+//echo $output;
 
-  $html_page = new DOMDocument;
-  libxml_use_internal_errors(true);
-  $html_page->loadHTML($output);
-  libxml_clear_errors();
+$html_page = new DOMDocument;
+libxml_use_internal_errors(true);
+$html_page->loadHTML($output);
+libxml_clear_errors();
 
-  $figures = $html_page->getElementsByTagName('figure');
+$figures = $html_page->getElementsByTagName('figure');
 
-  foreach($figures as $figure) {
+foreach($figures as $figure) {
 
-    $name = $figure->getElementsByTagName('span')[1]->nodeValue;
+  $name = $figure->getElementsByTagName('span')[1]->nodeValue;
 
-    if (in_array($name, $names_array)) {
-      $img_src = $figure->getElementsByTagName('img')[0]->getAttribute('src');
-      $profile_link = $figure->getElementsByTagName('a')[0]->getAttribute('href');
+  if (in_array($name, $names_array)) {
+    $img_src = $figure->getElementsByTagName('img')[0]->getAttribute('src');
+    $profile_link = $figure->getElementsByTagName('a')[0]->getAttribute('href');
 
-      echo '<a href="' . $profile_link . '" title="' . $name . '"><img src="' . $img_src . '"></a>';
-    }
-
-    //var_dump($profile_link);
-    //var_dump($img_src);
+    //echo '<a href="' . $profile_link . '" title="' . $name . '"><img src="' . $img_src . '"></a>';
+    echo json_encode([
+      'profile_link => $profile_link',
+      'name' => $name,
+    ]);
   }
 
+  //var_dump($profile_link);
+  //var_dump($img_src);
 }
